49381ed652f3b9479a6e4ef9d2509f342f1677af
Anant Pratap Singh <anant.abes459@gmail.com>
2024-05-16 21:54:54 +0530

    Anant

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState,useEffect,useCallback } from 'react'
 import Newsitem from './Newsitem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
